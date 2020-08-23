e55982b6000458ccc3f9b6c472d70e458023bb4b
drdineshrawat <dineshrawat7973@gmail.com>
2020-08-23 16:56:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import StaticDoughNutGraph from './Component/StaticDoughNutGraph';
 import GraphUsingHooks from './Component/GraphUsingHooks';
 // import DynamicGraph from './Component/DynamicGraph';
 
+// npm i --save react-chartjs-2
+// npm i --save chart.js
+
 function App() {
   return (
     <div className="App">
