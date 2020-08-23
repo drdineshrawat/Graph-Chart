cd8258ce9cf78897b4906ec6f8d4a45f8bdcde04
drdineshrawat <dineshrawat7973@gmail.com>
2020-08-23 17:57:35 +0530

    first commit

--- src/Component/DynamicGraph.js
+++ src/Component/DynamicGraph.js
@@ -1,38 +1,80 @@
 import React, { useState, useEffect } from 'react'
-import axious from 'axious'
+import axios from 'axios'
+import {Line}from 'react-chartjs-2'
 
 function DynamicGraph() {
   const [apiData, setApiData] = useState({})
     const [employeeSalary, setemployeeSalary] = useState([])
     const[employeeAge,setEmployeeAge]= useState([])
+    const[optionalData,setOptionalData]=useState({})
 
     useEffect(()=>{
         chart();
     },[])
 
   const chart=()=>{
-
-    axious.get("http://dummy.restapiexample.com/api/v1/employees")
+    let empSal=[];
+    let empAge=[];
+    axios.get("http://dummy.restapiexample.com/api/v1/employees")
     .then(response=>{
-         response.data
-    })
-    .catch(err=>{
-        console.log(err)
-        alert(err)
-    })
+        //  response.data.data
+        console.log(response.data.data)
+        for(const dataObj of response.data.data){
+            empSal.push(parseInt(dataObj.employee_salary))
+            empAge.push(parseInt(dataObj.employee_age))
+        }
 
-
-      setApiData({
-          labels:['','','','',''],
+        setApiData({
+          labels:empAge,
           datasets:[{
-
+              label:"Employee",
+              data:empSal,
+              backgroundColor:['rgba(75,192,192,0.6)'],
+              borderWidth:4,
           }]
       })
+ 
+    })
+    .catch(err=>{
+        console.log(err);
+        alert(err)
+    });
+console.log(empSal,empAge)
+
+
+
+setOptionalData({
+  resposive:true,
+  title:{
+      text:"Dinesh",
+      display:true,
+  },
+  scales:{
+      yAxes:[{
+          ticks:{
+              autoSkip:true,
+              maxTicksLimit:10,
+              beginAtZero:true,
+          },
+          gridLines:{
+              display:false,
+          }
+      }],
+      xAxes:[{
+          gridLines:{
+              display:false
+          }
+      }]
   }
+})
+
+
+}
+
 
     return (
         <div>
-            
+              <Line data={apiData} options={optionalData}/>
         </div>
     )
 }
